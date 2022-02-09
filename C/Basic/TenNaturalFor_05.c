1f2a1b3e7ab5f63e972736add21b1c93eedacc77
Pradyumna K Subudhi <mailtopksubudhi@gmail.com>
2022-02-09 17:35:25 +0530

    Add files via upload

---
+++ C/Basic/TenNaturalFor_05.c
@@ -0,0 +1,21 @@
+/*
+WAP to display initial 10 natural numbers side by side. i.e.
+
+	1 2 3 4 5 6 7 8 9 10
+	
+	P K Subudhi (WhatsApp: +91-8895174939, Mail: mailtopksubudhi@gmail.com)
+ 	Web: www.pksubudhi.com Call: +91-7008124637
+ 	Twitter: @pksubudhi4u
+	Mid Aug'21
+	
+*/
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		printf("%d ",i);
+	}
+}
+
