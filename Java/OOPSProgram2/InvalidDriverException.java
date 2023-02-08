9c7846c8e9411bad45e0fbee85211461bfea0f30
Pradyumna K Subudhi <mailtopksubudhi@gmail.com>
2023-02-08 16:26:01 +0530

    Add files via upload

---
+++ Java/OOPSProgram2/InvalidDriverException.java
@@ -0,0 +1,8 @@
+public class InvalidDriverException extends Exception {
+	public InvalidDriverException() {
+		super();
+	}
+	public InvalidDriverException(String message) {
+		super(message);
+	}
+}
