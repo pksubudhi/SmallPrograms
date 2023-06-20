cc9db9dd8c243e558592d71bde19d8c980889c59
P K Subudhi <mailtopksubudhi@gmail.com>
2023-06-20 15:05:16 +0530

    Add files via upload

---
+++ Python/DispAllPrimeInRange.py
@@ -0,0 +1,12 @@
+def main():
+    count = 0
+    y = 0
+    for n in range(2, 1000):
+        y = 0
+        for i in range(2, int(n/2)+1):
+            
+            if n%i == 0:
+                y = y + 1
+        if y == 0:
+            print(n, end=" ")
+main()
